ba0d67c6f0559f96fbf0b2e788fc04a6dc70dda0
jinal74 <jinalmathukiya74@gmail.com>
2021-12-13 08:04:47 +0530

    op_completed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
 import './App.css';
-import Country from './containers/Country';
-import State from './containers/State';
-import City from './containers/City';
 
+// function App() {
+//   let arr =[10,20,30,40,50]
+//   let arr1 =[...arr]
+//   let arr2 =[100,200,300,...arr1,400,500]
+//   return (
+//     arr2
+//     .map((a,i) => {
+//       return (
+//         <div key={i}>
+//         <p>{a}</p>
+//         </div>
+//       )
+//     })
 
+//   );
+// }
 function App() {
-  
-  return (
-    <>
-      <Country />
-      <State />
-      <City />
-    </>
+  let obj1 = {
+    id : 101 , 
+    name:"RADHIKA"
+  }
 
-  );
+  let obj2= {...obj1}
+
+  let obj3 = {
+    "salary" : 50000, 
+    ...obj2,
+    "city":"surat"
+  }
+  let obj4 = {
+    "salary" : 50000, 
+    "age" : 25,
+    ...obj2,
+    "city":"surat"
+  }
+    return(
+      <>
+        {
+        <div>
+          <p>{obj3.id}</p>
+          <p>{obj3.name}</p>
+          <p>{obj3.city}</p>
+          <p>{obj3.salary}</p>
+          <p>{obj3.sal}</p>
+        </div>
+      }
+      </>
+    );
 }
 
 export default App; 
